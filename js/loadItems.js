50636fc81467b0860780946e84eee4a62cf17725
landolil <landolil@hotmail.com>
2024-07-05 19:27:53 -0700

    u

--- js/loadItems.js
+++ js/loadItems.js
@@ -2,11 +2,17 @@
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 
 // js/loaditems.js
 
 =======
 >>>>>>> parent of ddbc44d (fix menue)
+=======
+
+// js/loaditems.js
+
+>>>>>>> parent of ff621ce (Menue)
 =======
 
 // js/loaditems.js
@@ -133,6 +139,7 @@ function loadItems(items, type, phylum = null) {
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 function initialize() {
     if (sidebar && mainImage && details && navBar && data && data.phyla) {
@@ -144,6 +151,8 @@ function initialize() {
 }
 
 >>>>>>> parent of ddbc44d (fix menue)
+=======
+>>>>>>> parent of ff621ce (Menue)
 =======
 >>>>>>> parent of ff621ce (Menue)
 =======
